63800048e88c2a9e038669e1ed75baec6b0a5e86
Alejandro <130703074+Tineo06@users.noreply.github.com>
2025-12-01 10:21:07 +0100

    base de datos turso

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,10 +1,31 @@
 <?php
 
-$Repit=false;
-$host="localhost";
-$user="root";
-$password="";
+function turso_execute($sql, $params = []) {
+    $url = getenv("libsql://database-teal-chair-vercel-icfg-ghadoh688jz7a6onefrjbbh9.aws-us-east-1.turso.io") . "/execute";
+    $token = getenv("eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NjQ1ODAxNzQsImlkIjoiM2RjODgxYjMtNzZjZC00YjY5LTgyZWYtMmY3MmIwMzFlOWQ4IiwicmlkIjoiOTA0MmE2NzYtNzRhMS00MmM3LWEwM2UtOTRmNDg1OGUyZTQ1In0.cfWQQ8rEKPPZDQbdcbyoInR9L80UA5HkW-Cme1CbjsEPhUHkCwikJ0r71gvcBzexpYVKXb_xjFfHZUace11hDg");
 
-$link= mysqli_connect($host,$user,$password);
-$tildes=$link->query("SET NAMES 'utf8'");
-mysqli_select_db($link,'periodicos');
+    $payload = [
+        "statements" => [
+            [
+                "sql" => $sql,
+                "params" => $params
+            ]
+        ]
+    ];
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Authorization: Bearer $token",
+        "Content-Type: application/json"
+    ]);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($result, true);
+}
+?>
